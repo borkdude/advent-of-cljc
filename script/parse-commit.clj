269275461a35bee30bd5c9ade519c1d9e48d2250
Michiel Borkent <michielborkent@gmail.com>
2018-12-13 10:36:08 +0100

    Enhance diff script

--- script/parse-commit.clj
+++
@@ -1,4 +0,0 @@
-(let [commit-message (first *command-line-args*)
-      [year day user] (rest (re-find #"(\d{4})[ /,](\d{2})[ /,](\w+)" commit-message))]
-  (assert (and year day user) "Please provide your commit message in the format yyyy/dd/user ...")
-  (println year day user))
